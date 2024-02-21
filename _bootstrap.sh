bc2388ff7737de2c6c76e7fca56063abde73bad9
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-02-20 20:44:14 -0600

    setup basic structure

---
+++ _bootstrap.sh
@@ -0,0 +1,18 @@
+# bail if we hit an error
+set -e
+
+# update indexes
+apt-get update
+
+# install Python3 and pip3
+apt-get install python3 python3-pip
+
+# Use pip to install ansible
+pip3 install --user ansible
+
+# If pip binary path is not on the PATH var, add it to profile file
+echo $PATH | grep "\.local" > /dev/null || echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
+
+
+# If pip binary path is not on the PATH var, add it PATH for this session
+echo $PATH | grep "\.local" > /dev/null || export PATH=$PATH:~/.local/bin
